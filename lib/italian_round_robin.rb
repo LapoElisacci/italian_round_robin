3258d3012714ce3d01822539c1fc4eeede27cf5a
Lapo <lapoelisacci@gmail.com>
2023-08-25 14:56:05 +0200

    chore: Rubocop

--- lib/italian_round_robin.rb
+++ lib/italian_round_robin.rb
@@ -4,7 +4,7 @@ require_relative "italian_round_robin/version"
 
 module ItalianRoundRobin
   class Error < StandardError; end
-  
+
   class << self
     def generate(members)
       @@members = members
@@ -20,13 +20,13 @@ module ItalianRoundRobin
       # Generate the remaining rounds
       (2...@@size).each do |gyros|
         # Remove and save the constant player index
-        constant_index = indexes.delete_at(@@size-1)
+        constant_index = indexes.delete_at(@@size - 1)
         # Move in the first half of the list to the end of it
         indexes.concat(indexes[0...@@half])
         # Save the second half to the first one
-        indexes[0...@@half - 1] = indexes[@@half...@@size-1]
+        indexes[0...@@half - 1] = indexes[@@half...@@size - 1]
         # Remove double entries
-        indexes.slice!(@@half-1...@@size-1)
+        indexes.slice!(@@half - 1...@@size - 1)
         # Add the constant player index
         indexes.append(constant_index)
         # Generate the round
@@ -37,26 +37,25 @@ module ItalianRoundRobin
 
     private
 
-      def generate_round(round, indexes)
-        matches = []
-        start_index = 0
+    def generate_round(round, indexes)
+      matches = []
+      start_index = 0
 
-        # In even round the highest index player gets White 
-        if round.even?
-          white = @@members[indexes[@@size - 1] - 1]
-          black = @@members[indexes[0] - 1]
-          matches << [white, black]
-          start_index = 1
-        end
-
-        (start_index...@@half).each do |i|
-          white = @@members[indexes[i] - 1]
-          black = @@members[indexes[@@size - 1 - i] - 1]
-          matches << [white, black]
-        end
-
-        matches
+      # In even round the highest index player gets White
+      if round.even?
+        white = @@members[indexes[@@size - 1] - 1]
+        black = @@members[indexes[0] - 1]
+        matches << [white, black]
+        start_index = 1
       end
+
+      (start_index...@@half).each do |i|
+        white = @@members[indexes[i] - 1]
+        black = @@members[indexes[@@size - 1 - i] - 1]
+        matches << [white, black]
+      end
+
+      matches
+    end
   end
 end
-
